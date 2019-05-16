e9e6d2d91acdb9dfdd39f8764f605ea63fa44c86
Patrick Cuba <cubap@slu.edu>
2019-05-16 10:02:06 -0500

    paste event instead of input

--- js/deer-record.js
+++ js/deer-record.js
@@ -95,7 +95,7 @@ export default class DeerReport {
                                     for (const v of assertedValue) {
                                         if(!el.value && (["string","number"].indexOf(typeof v)!==-1)){
                                             el.value = v
-                                            el.dispatchEvent(new Event('input'));
+                                            el.dispatchEvent(new Event('paste'));
                                         }
                                         if(typeof v === "object") {
                                             
@@ -103,7 +103,7 @@ export default class DeerReport {
                                     }
                                 } else {
                                     el.value = UTILS.getValue(obj[key])
-                                    el.dispatchEvent(new Event('input'));
+                                    el.dispatchEvent(new Event('paste'));
                                 }
                                 if(obj[key].source) {
                                     el.setAttribute(DEER.SOURCE,UTILS.getValue(obj[key].source,"citationSource"))
