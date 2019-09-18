97bca07eec97babbb5565fdeb071224c51916426
Bryan Haberberger <bhaberbe@slu.edu>
2019-09-18 14:51:59 -0500

    Have warnings turned on by default.

--- js/deer-config.js
+++ js/deer-config.js
@@ -43,7 +43,7 @@ export default {
     SUPPRESS: ["__rerum","@context"],   //properties to ignore
     ATTRIBUTION: "testMachine",         //replace with user to attribute assertions
     DELIMETERDEFAULT: ",",              //Default delimeter for .split()ing and .join()ing 
-    ROBUSTFEEDBACK : false,              //Show warnings along with errors in the web console.  Set to false to only see errors.  
+    ROBUSTFEEDBACK : true,              //Show warnings along with errors in the web console.  Set to false to only see errors.  
 
     /**
      * Add any custom templates here through import or copy paste.
