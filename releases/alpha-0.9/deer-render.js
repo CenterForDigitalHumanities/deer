0917fdb8c332c58409ee7831c40fef9c12b21a17
Patrick Cuba <cubap@slu.edu>
2019-10-18 08:49:54 -0500

    release 0.9

---
+++ releases/alpha-0.9/deer-render.js
@@ -0,0 +1,307 @@
+/**
+ * @module DeerRender Data Encoding and Exhibition for RERUM
+ * @author Patrick Cuba <cubap@slu.edu>
+ * @author Bryan Haberberger <bryan.j.haberberger@slu.edu>
+ * @version 0.7
+
+
+ * This code should serve as a basis for developers wishing to
+ * use TinyThings as a RERUM proxy for an application for data entry,
+ * especially within the Eventities model.
+ * @see tiny.rerum.io
+ */
+
+import { default as UTILS } from './deer-utils.js'
+import { default as config } from './deer-config.js'
+
+const changeLoader = new MutationObserver(renderChange)
+var DEER = config
+
+/**
+ * Observer callback for rendering newly loaded objects. Checks the
+ * mutationsList for "deep-object" attribute changes.
+ * @param {Array} mutationsList of MutationRecord objects
+ */
+async function renderChange(mutationsList) {
+    for (var mutation of mutationsList) {
+        switch (mutation.attributeName) {
+            case DEER.ID:
+            case DEER.KEY:
+            case DEER.LINK:
+            case DEER.LIST:
+            let id = mutation.target.getAttribute(DEER.ID)
+            if (id === "null" || mutation.target.getAttribute(DEER.COLLECTION)) return
+            let obj = {}
+            try {
+                obj = JSON.parse(localStorage.getItem(id))
+            } catch (err) {}
+            if (!obj||!obj["@id"]) {
+                obj = await fetch(id).then(response => response.json()).catch(error => error)
+                if (obj) {
+                    localStorage.setItem(obj["@id"] || obj.id, JSON.stringify(obj))
+                } else {
+                    return false
+                }
+            }
+            RENDER.element(mutation.target,obj)
+            break
+            case DEER.LISTENING:
+            let listensTo = mutation.target.getAttribute(DEER.LISTENING)
+            if(listensTo){
+                mutation.target.addEventListener(DEER.EVENTS.CLICKED,e=>{
+                    let loadId = e.detail["@id"]
+                    if(loadId===listensTo) { mutation.target.setAttribute("deer-id",loadId) }
+                })
+            }
+		}
+	}
+}
+
+const RENDER = {}
+
+RENDER.element = function(elem,obj) {
+    
+    return UTILS.expand(obj).then(obj=>{
+        let tmplName = elem.getAttribute(DEER.TEMPLATE) || (elem.getAttribute(DEER.COLLECTION) ? "list" : "json")
+        let template = DEER.TEMPLATES[tmplName] || DEER.TEMPLATES.json
+        let options = {
+            list: elem.getAttribute(DEER.LIST),
+            link: elem.getAttribute(DEER.LINK),
+            collection: elem.getAttribute(DEER.COLLECTION),
+            key: elem.getAttribute(DEER.KEY),
+            label: elem.getAttribute(DEER.LABEL),
+            config: DEER
+        }
+        let templateResponse = template(obj,options)
+
+        elem.innerHTML = (typeof templateResponse.html==="string") ? templateResponse.html : templateResponse
+
+        let newViews = elem.querySelectorAll(config.VIEW)
+        if(newViews.length>0) {
+            UTILS.broadcast(undefined,DEER.EVENTS.NEW_VIEW,elem,{set:newViews})
+        }
+        let newForms = elem.querySelectorAll(config.FORM)
+        if(newForms.length > 0) {
+            UTILS.broadcast(undefined,DEER.EVENTS.NEW_FORM,elem,{set:newForms})
+        }
+
+        if (typeof templateResponse.then === "function") { templateResponse.then(elem,obj,options) }
+
+        UTILS.broadcast(undefined,DEER.EVENTS.LOADED,elem,obj)
+    })
+}
+
+/**
+ * The TEMPLATED renderer to draw JSON to the screen
+ * @param {Object} obj some json to be drawn as JSON
+ * @param {Object} options additional properties to draw with the JSON
+ */
+DEER.TEMPLATES.json = function(obj, options={}) {
+    let indent = options.indent || 4
+    let replacer = (k,v) => {
+        if(DEER.SUPPRESS.indexOf(k) !== -1) return
+        return v
+    }
+    try {
+        return `<pre>${JSON.stringify(obj, replacer, indent)}</pre>`
+    } catch (err) {
+        return null
+    }
+}
+
+/**
+ * Get a certain property from an object and return it formatted as HTML to be drawn.  
+ * @param {Object} obj some obj containing a key that needs to be drawn
+ * @param {String} key the name of the key in the obj we are looking for
+ * @param {String} label The label to be displayed when drawn
+ */
+DEER.TEMPLATES.prop= function(obj, options = {}) {
+    let key = options.key || "@id"
+    let prop = obj[key]
+    let label = options.label || UTILS.getLabel(obj,prop)
+    try {
+        return `<span class="${prop}">${label}: ${UTILS.getValue(prop) || "[ undefined ]"}</span>`
+    } catch (err) {
+        return null
+    }
+}
+
+/**
+ * The TEMPLATED renderer to draw an JSON to the screen as some HTML template
+ * @param {Object} obj some json of type Entity to be drawn
+ * @param {Object} options additional properties to draw with the Entity
+ */
+DEER.TEMPLATES.entity= function(obj, options = {}) {
+    let tmpl = `<h2>${UTILS.getLabel(obj)}</h2>`
+    let list = ``
+    
+    for (let key in obj) {
+        if(DEER.SUPPRESS.indexOf(key)>-1) {continue}
+        let label = key
+        let value = UTILS.getValue(obj[key],key)
+        try {
+            if ((value.image || value.trim()).length > 0) {
+                list += (label === "depiction") ? `<img title="${label}" src="${value.image || value}" deer-source="${obj[key].source}">` : `<dt deer-source="${obj[key].source}">${label}</dt><dd>${value.image || value}</dd>`
+            }
+        } catch (err) {
+            // Some object maybe or untrimmable somesuch
+            // is it object/array?
+            list+=`<dt>${label}</dt>`
+            if(Array.isArray(value)){
+                value.forEach((val,index)=>{
+                    let name = UTILS.getLabel(val,(val.type || val['@type'] || label+index))
+                    list+= (val["@id"]) ? `<dd><a href="#${val["@id"]}">${name}</a></dd>` : `<dd>${name}</dd>`
+                })
+            } else {
+                // a single, probably
+                let v = UTILS.getValue(value)
+                if(typeof v==="object") { v = UTILS.getLabel(v) }
+                if(v === "[ unlabeled ]") { v = v['@id'] || v.id || "[ complex value unknown ]"}
+                list+=(value['@id'])?`<dd><a href="${options.link||""}#${value['@id']}">${v}</a></dd>`:`<dd>${v}</dd>`
+            }
+        }
+    }
+    tmpl += (list.includes("<dd>")) ? `<dl>${list}</dl>` : ``
+    return tmpl
+}
+
+DEER.TEMPLATES.list= function(obj, options={}) {
+    let tmpl = `<h2>${UTILS.getLabel(obj)}</h2>`
+    if(options.list){
+        tmpl += `<ul>`
+        obj[options.list].forEach((val,index)=>{
+            let name = UTILS.getLabel(val,(val.type || val['@type'] || index))
+            tmpl+= (val["@id"] && options.link) ? `<li ${DEER.ID}="${val["@id"]}"><a href="${options.link}${val["@id"]}">${name}</a></li>` : `<li ${DEER.ID}="${val["@id"]}">${name}</li>`
+        })
+        tmpl += `</ul>`
+    }
+    
+    return tmpl
+}
+/**
+ * The TEMPLATED renderer to draw JSON to the screen
+ * @param {Object} obj some json of type Person to be drawn
+ * @param {Object} options additional properties to draw with the Person
+ */
+DEER.TEMPLATES.person= function(obj, options={}) {
+    try {
+        let tmpl = `<h2>${UTILS.getLabel(obj)}</h2>`
+        let dob = DEER.TEMPLATES.prop(obj, {key:"birthDate", title:"Birth Date"}) || ``
+        let dod = DEER.TEMPLATES.prop(obj, {key:"deathDate", title:"Death Date"}) || ``
+        let famName = (obj.familyName&&UTILS.getValue(obj.familyName))||"[ unknown ]"
+        let givenName = (obj.givenName&&UTILS.getValue(obj.givenName))||""
+        tmpl += (obj.familyName||obj.givenName) ? `<div>Name: ${famName}, ${givenName}</div>` : ``
+        tmpl += dob + dod
+        tmpl += `<a href="#${obj["@id"]}">${name}</a>`
+        return tmpl
+    } catch (err) {
+        return null
+    }
+    return null
+}
+/**
+ * The TEMPLATED renderer to draw JSON to the screen
+ * @param {Object} obj some json of type Event to be drawn
+ * @param {Object} options additional properties to draw with the Event
+ */
+DEER.TEMPLATES.event= function(obj, options={}) {
+    try {
+        let tmpl = `<h1>${UTILS.getLabel(obj)}</h1>`
+        return tmpl
+    } catch (err) {
+        return null
+    }
+    return null
+}
+
+export default class DeerRender {
+    constructor(elem, deer={}){
+        for(let key in DEER) {
+            if(typeof DEER[key] === "string") {
+                DEER[key] = deer[key] || config[key]
+            } else {
+                DEER[key] = Object.assign(config[key],deer[key])
+            }
+        }
+        changeLoader.observe(elem, {
+            attributes:true
+        })
+        this.$dirty = false
+        this.id = elem.getAttribute(DEER.ID)
+        this.collection = elem.getAttribute(DEER.COLLECTION)
+        this.elem = elem
+        
+        try {
+            if(!(this.id||this.collection)){
+                let err = new Error(this.id+" is not a valid id.")
+                err.code = "NO_ID"
+                throw err
+            } else {
+                if(this.id) {
+                    fetch(this.id).then(response=>response.json()).then(obj=>RENDER.element(this.elem,obj)).catch(err=>err)
+                } else if (this.collection) {
+                    // Look not only for direct objects, but also collection annotations
+                    let queryObj = {
+                        $or: [{
+                            "targetCollection": this.collection
+                        },{
+                            "body.targetCollection": this.collection
+                        }]
+                    }
+                    fetch(DEER.URLS.QUERY, {
+                        method: "POST",
+                        mode: "cors",
+                        body: JSON.stringify(queryObj)
+                    }).then(response => response.json())
+                    .then(pointers => {
+                        let list = []
+                        pointers.map(tc => list.push(fetch(tc.target || tc["@id"] || tc.id).then(response=>response.json().catch(err=>{__deleted:console.log(err)}))))
+                         return Promise.all(list).then(l=>l.filter(i=>!i.hasOwnProperty("__deleted")))
+
+                    })
+                    .then(list => {
+                        let listObj = {
+                            name: this.collection,
+                            itemListElement: list
+                        }
+                        this.elem.setAttribute(DEER.LIST,"itemListElement")
+                        try {
+                            listObj["@type"] = list[0]["@type"] || list[0].type || "ItemList"
+                        } catch (err) {}
+                        RENDER.element(this.elem,listObj)
+                    })
+                }
+            }
+        } catch(err){
+            let message = err
+            switch(err.code){
+                case "NO_ID": message=`` // No DEER.ID, so leave it blank
+            }
+            elem.innerHTML = message
+        }
+
+        let listensTo = elem.getAttribute(DEER.LISTENING)
+        if(listensTo){
+            elem.addEventListener(DEER.EVENTS.CLICKED,e=> {
+                try{
+                    if(e.detail.target.closest(DEER.VIEW+","+DEER.FORM).getAttribute("id")===listensTo) elem.setAttribute(DEER.ID,e.detail.target.closest('['+DEER.ID+']').getAttribute(DEER.ID))
+                } catch (err) {}
+            })
+            try{
+                window[listensTo].addEventListener("click", e => UTILS.broadcast(e,DEER.EVENTS.CLICKED,elem))
+            }
+            catch (err) {
+                console.error("There is no HTML element with id "+listensTo+" to attach an event to");
+            }
+            
+        }
+        
+    }
+}
+
+export function initializeDeerViews(config) {
+    const views = document.querySelectorAll(config.VIEW)
+    Array.from(views).forEach(elem=>new DeerRender(elem,config))
+    document.addEventListener(DEER.EVENTS.NEW_VIEW,e => Array.from(e.detail.set).forEach(elem=>new DeerRender(elem,config)))
+}
+
---
+++ releases/alpha-0.9/deer-utils.js
@@ -0,0 +1,264 @@
+/**
+ * @module DEER Data Encoding and Exhibition for RERUM
+ * @author Patrick Cuba <cubap@slu.edu>
+ * @author Bryan Haberberger <bryan.j.haberberger@slu.edu>
+ * @version 0.7
+
+ * This code should serve as a basis for developers wishing to
+ * use TinyThings as a RERUM proxy for an application for data entry,
+ * especially within the Eventities model.
+ * @see tiny.rerum.io
+ */
+
+import { default as DEER } from './deer-config.js'
+
+export default {
+    listFromCollection: function (collectionId) {
+        let queryObj = {
+            body: {
+                targetCollection: collectionId
+            }
+        }
+        return fetch(DEER.URLS.QUERY, {
+            method: "POST",
+            body: JSON.stringify(queryObj)
+        }).then(response => response.json())
+            .then(function (pointers) {
+                let list = []
+                pointers.map(tc => list.push(fetch(tc.target).then(response => response.json())))
+                return Promise.all(list)
+            })
+            .then(function (list) {
+                return list
+            })
+    },
+    listFromContainer: function () { },
+    getValue: function (property, alsoPeek = [], asType) {
+        // TODO: There must be a best way to do this...
+        let prop;
+        if (property === undefined || property === "") {
+            console.error("Value of property to lookup is missing!")
+            return undefined
+        }
+        if (Array.isArray(property)) {
+            prop = property.map(this.getValue.bind(this))
+        } else {
+            if (typeof property === "object") {
+                // TODO: JSON-LD insists on "@value", but this is simplified in a lot
+                // of contexts. Reading that is ideal in the future.
+                if (!Array.isArray(alsoPeek)) {
+                    alsoPeek = [alsoPeek]
+                }
+                alsoPeek = alsoPeek.concat(["@value", "value", "$value", "val"])
+                for (let k of alsoPeek) {
+                    if (property.hasOwnProperty(k)) {
+                        prop = property[k]
+                        break
+                    }
+                    else {
+                        prop = property
+                    }
+                }
+            }
+            else {
+                prop = property
+            }
+        }
+        try {
+            switch (asType.toUpperCase()) {
+                case "STRING":
+                    prop = prop.toString();
+                    break
+                case "NUMBER":
+                    prop = parseFloat(prop);
+                    break
+                case "INTEGER":
+                    prop = parseInt(prop);
+                    break
+                case "BOOLEAN":
+                    prop = !Boolean(["false", "no", "0", "", "undefined", "null"].indexOf(String(prop).toLowerCase().trim()));
+                    break
+                default:
+            }
+        } catch (err) {
+            if (asType) {
+                throw new Error("asType: '" + asType + "' is not possible.\n" + err.message)
+            } else {
+                // no casting requested
+            }
+        } finally {
+            return (prop.length === 1) ? prop[0] : prop
+        }
+    },
+    /**
+     * Attempt to discover a readable label from the object
+     */
+    get getLabel() {
+        return (obj, noLabel = "[ unlabeled ]", options = {}) => {
+            if (typeof obj === "string") { return obj }
+            let label = obj[options.label] || obj.name || obj.label || obj.title
+            if(Array.isArray(label)) {
+                label = [...new Set(label.map(l => this.getValue(this.getLabel(l))))]
+
+            }
+            return label || noLabel
+        }
+    },
+    /**
+     * Take a known object with an id and query for annotations targeting it.
+     * Discovered annotations are attached to the original object and returned.
+     * @param {Object} entity Target object to search for description
+     */
+    async expand(entity) {
+        let findId = entity["@id"] || entity.id || entity
+        if (typeof findId !== "string") {
+            console.warn("Unable to find URI in object:",entity)
+            return entity
+        }
+        let getValue = this.getValue
+        return fetch(findId).then(response => response.json())
+            .then(obj => this.findByTargetId(findId)
+                .then(function (annos) {
+                    for (let i = 0; i < annos.length; i++) {
+                        let body
+                        try {
+                            body = annos[i].body
+                        } catch (err) { continue }
+                        if (!body) { continue }
+                        if (!Array.isArray(body)) {
+                            body = [body]
+                        }
+                        Leaf: for (let j = 0; j < body.length; j++) {
+                            if (body[j].evidence) {
+                                obj.evidence = (typeof body[j].evidence === "object") ? body[j].evidence["@id"] : body[j].evidence;
+                            }
+                            else {
+                                try {
+                                    let val = body[j];
+                                    let k = Object.keys(val)[0];
+                                    if (!val.source) {
+                                        // include an origin for this property, placehold madsrdf:Source
+                                        let aVal = getValue(val[k]);
+                                        val[k] = {
+                                            value: aVal,
+                                            source: {
+                                                citationSource: annos[i]["@id"],
+                                                citationNote: annos[i].label || "Composed object from DEER",
+                                                comment: "Learn about the assembler for this object at https://github.com/CenterForDigitalHumanities/TinyThings"
+                                            }
+                                        };
+                                    }
+                                    if (obj[k] !== undefined && annos[i].__rerum && annos[i].__rerum.history.next.length) {
+                                        // this is not the most recent available
+                                        // TODO: maybe check generator, etc.
+                                        continue Leaf;
+                                    }
+                                    else {
+                                        // Assign this to the main object.
+                                        if(obj[k]) {
+                                            // It may be already there as an Array with some various labels
+                                            if (Array.isArray(obj[k])){
+                                                let deepMatch = false
+                                                for(const e of obj[k]) {
+                                                    if(e.name===val.name){
+                                                        deepMatch = true
+                                                        break
+                                                    }
+                                                }
+                                                if(!deepMatch) { obj[k].push(val) }
+                                            } else if (obj[k].name !== val.name) { // often undefined
+                                                obj[k] = [obj[k],val]
+                                            }
+                                        } else {
+                                            // or just tack it on
+                                            obj = Object.assign(obj, val);
+                                        }
+                                    }
+                                }
+                                catch (err_1) { }
+                            }
+                        }
+                    }
+                    return obj
+                })).catch(err => {
+                    console.log("Error expanding object:" + err)
+                    return err
+                })
+    },
+    /**
+     * Execute query for any annotations in RERUM which target the
+     * id passed in. Promise resolves to an array of annotations.
+     * @param {String} id URI for the targeted entity
+     * @param [String] targetStyle other formats of resource targeting.  May be null
+     */
+    findByTargetId: async function (id, targetStyle=[]) {
+        let everything = Object.keys(localStorage).map(k => JSON.parse(localStorage.getItem(k)))
+        if (!Array.isArray(targetStyle)) {
+            targetStyle = [targetStyle]
+        }
+        targetStyle = targetStyle.concat(["target", "target.@id", "target.id"]) //target.source?
+        let obj = {"$or":[]}
+        for (let target of targetStyle) {
+            //Entries that are not strings are not supported.  Ignore those entries.  
+            //TODO: should we we let the user know we had to ignore something here?
+            if(typeof target === "string"){
+                let o = {}
+                o[target] = id
+                obj["$or"].push(o)
+            }
+        }
+        let matches = await fetch(DEER.URLS.QUERY, {
+            method: "POST",
+            body: JSON.stringify(obj),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .catch((err) => console.log(err))
+        let local_matches = everything.filter(o => o.target === id)
+        matches = local_matches.concat(matches)
+        return matches
+    },
+
+    /**
+     * An error handler for various HTTP traffic scenarios
+     */
+    handleHTTPError: function (response) {
+        if (!response.ok) {
+            let status = response.status
+            switch (status) {
+                case 400:
+                    console.log("Bad Request")
+                    break
+                case 401:
+                    console.log("Request was unauthorized")
+                    break
+                case 403:
+                    console.log("Forbidden to make request")
+                    break
+                case 404:
+                    console.log("Not found")
+                    break
+                case 500:
+                    console.log("Internal server error")
+                    break
+                case 503:
+                    console.log("Server down time")
+                    break
+                default:
+                    console.log("unahndled HTTP ERROR")
+            }
+            throw Error("HTTP Error: " + response.statusText)
+        }
+        return response
+    },
+
+    /**
+     * Broadcast a message about DEER
+     */
+    broadcast: function (event = {}, type, element, obj = {}) {
+        let e = new CustomEvent(type, { detail: Object.assign(obj, { target: event.target }), bubbles: true })
+        element.dispatchEvent(e)
+    }
+}
