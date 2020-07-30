8358fdea93ff46a83169511e24c01b14186fb7f4
Patrick Cuba <cubap@slu.edu>
2020-07-30 11:33:41 -0500

    setting up worker

---
+++ js/worker.js
@@ -0,0 +1,45 @@
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/pages/offline.html',
+    '/pages/404.html'
+  ]
+
+const staticCacheName = 'deer-cache-v1'
+
+self.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets')
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then(cache => {
+                return cache.addAll(filesToCache)
+            })
+    )
+})
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url)
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log('Found ', event.request.url, ' in cache')
+                    return response
+                }
+                console.log('Network request for ', event.request.url)
+                return fetch(event.request)
+                    .then(response => {
+                        if (response.status === 404) {
+                            return caches.match('pages/404.html')
+                        }
+                        return caches.open(staticCacheName).then(cache => {
+                            cache.put(event.request.url, response.clone())
+                            return response
+                        })
+                    })
+            }).catch(error => {
+                console.log('Error, ', error)
+                return caches.match('/pages/offline.html')
+            })
+    )
+})
