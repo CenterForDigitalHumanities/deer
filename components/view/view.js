bdd02bb056327e9e14dbb1b295062b4ffd5b1cf9
cubap <cubap@slu.edu>
2022-08-17 14:39:40 -0500

    Update view.js

--- components/view/view.js
+++ components/view/view.js
@@ -25,8 +25,7 @@ export default class DeerView extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `<small>&copy;2022 Research Computing Group</small>`
         this.addEventListener('message', e => this.innerHTML+=`msg!`)
-        // UTILS.worker.
-        addEventListener('message', e => {
+        UTILS.worker.addEventListener('message', e => {
             if (e.data.id !== this.getAttribute(DEER.ID)) { return }
             switch (e.data.action) {
                 case "update":
