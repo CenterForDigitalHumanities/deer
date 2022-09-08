7abd34937cdbeea06ebbc11fff98edab0fa6bdaf
cubap <cubap@slu.edu>
2022-09-08 15:09:26 -0500

    absolute for releases

--- releases/rc-1.0/components/templates/default.js
+++ releases/rc-1.0/components/templates/default.js
@@ -9,7 +9,7 @@
  * @tags templates, deer, rerum
  */
 
-import { UTILS, DEER } from '/js/deer-utils.js'
+import { UTILS, DEER } from '//deer.rerum.io/releases/rc-1.0/js/deer-utils.js'
 
 /**
  * Get a certain property from an object and return it formatted as HTML to be drawn.  
